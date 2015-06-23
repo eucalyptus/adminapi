6db5e25a7639f70ba032d7f4de32cdd6fd46e2c1
Matt Clark <mclark@eucalyptus.com>
2015-06-22 22:16:50 -0700

    Move sctp tools to ip_rx and ip_tx

--- cloud_utils/net_utils/ip_rx.py
+++ cloud_utils/net_utils/ip_rx.py
@@ -4,19 +4,26 @@
 # Simple SCTP server/listener
 #
 '''
+import copy
 import select
 import socket
 import sys
+import json
 import struct
 import time
 from optparse import OptionParser
 
 
 parser = OptionParser()
-parser.add_option("-p", "--ports", dest="destports", default='8773',
-                  help="Comma separated list of Destination Ports to filter on", metavar='PORT')
 
-parser.add_option("-f", "--sports", dest="srcports", default='8773',
+parser.add_option("-n", "--testname", dest="testname", default=str(time.asctime()),
+                  help="Name used to identify test results", metavar='TESTNAME')
+
+parser.add_option("-p", "--dst-ports", dest="destports", default='',
+                  help="Comma separated list of Destination Ports to filter on, example:8773",
+                  metavar='PORT')
+
+parser.add_option("-r", "--src-ports", dest="srcports", default="",
                   help="Comma separated list of Source Ports to filter on", metavar='PORT')
 
 parser.add_option("-o", "--proto", dest="proto", type="int", default=17,
@@ -36,7 +43,8 @@ parser.add_option("-s", "--src-addrs", dest="srcaddrs", default="",
                   help="Comma delimited list of src ip addresses used to filter", metavar="ADDRS")
 
 parser.add_option("-d", "--dst-addrs", dest="dstaddrs", default="",
-                  help="Comma delimited list of dst ip addresses used to filter", metavar="ADDRS")
+                  help="Comma delimited list of dst ip addresses used to filter, "
+                       "example: 228.7.7.3", metavar="ADDRS")
 
 parser.add_option("--bind", dest="bind", action='store_true', default=False,
                   help="Flag to enable port binding, default:false")
@@ -46,6 +54,9 @@ parser.add_option("-q", "--quiet", dest="quiet", action='store_true', default=Fa
 
 parser.add_option("-a", "--addr", dest="addr", default='',
                   help="Local addr to bind to, default is '' or 'listen on all'", metavar='HOST')
+
+parser.add_option("-f", "--file", dest="resultsfile", default="",
+                  help="File Path to save results to", metavar="ADDRS")
 options, args = parser.parse_args()
 
 HOST = options.addr
@@ -54,27 +65,33 @@ BIND = options.bind
 COUNT = options.count
 QUIET = options.quiet
 TIMEOUT = options.timeout
-DSTPORTS = []
+DSTPORTS = {}
 if options.destports:
     for port in options.destports.split(','):
-        DSTPORTS.append(int(port))
+        DSTPORTS[int(port)] = 0
 if len(DSTPORTS) > 1 and BIND:
     raise ValueError('Cannot use BIND option with more than one port at this time')
-SRCPORTS = []
+SRCPORTS = {}
 if options.srcports:
     for port in options.srcports.split(','):
-        SRCPORTS.append(int(port))
-SRCADDRS = []
+        SRCPORTS[int(port)] = 0
+SRCADDRS = {}
 if options.srcaddrs:
     for addr in options.srcaddrs.split(','):
-        SRCADDRS.append(str(addr).strip())
-DSTADDRS = []
+        SRCADDRS[str(addr).strip()] = {}
+DSTADDRS = {}
 if options.dstaddrs:
     for addr in options.dstaddrs.split(','):
-        DSTADDRS.append(str(addr).strip())
-#print 'Using srcaddrs:{0}, and timeout:'.format(SRCADDRS, TIMEOUT)
+        DSTADDRS[str(addr).strip()] = {}
+results = {'packets': {}, 'elapsed': None, 'count': None, 'name': options.testname}
+sock = None
+file = None
 
-#HOST = socket.gethostbyname(socket.gethostname())
+def debug(msg):
+    for line in str(msg).splitlines():
+        print "# {0}".format(str(line))
+
+
 
 class IPHdr(object):
     def __init__(self, packet):
@@ -87,36 +104,34 @@ class IPHdr(object):
         self.parse_ip_hdr(packet)
 
     def parse_ip_hdr(self, packet):
-        #take first 20 characters for the ip header
         ip_header = packet[0:20]
-
-        #now unpack them :)
         iph = struct.unpack('!BBHHHBBH4s4s' , ip_header)
-        # print "IPH: {0}".format(iph)
-
         version_ihl = iph[0]
         self.version = version_ihl >> 4
         ihl = version_ihl & 0xF
-
         self.header_len = ihl * 4
-
         self.ttl = iph[5]
         self.protocol = iph[6]
         self.src_addr = socket.inet_ntoa(iph[8]);
         self.dst_addr = socket.inet_ntoa(iph[9]);
 
     def print_me(self):
-        print "IP ver:{0}, HDR LEN:{1}, TTL:{2}, PROTO:{3}, SRC ADDR:{4}, DST ADDR:{5}"\
+        debug("IP ver:{0}, HDR LEN:{1}, TTL:{2}, PROTO:{3}, SRC ADDR:{4}, DST ADDR:{5}"
             .format(self.version, self.header_len, self.ttl, self.protocol, self.src_addr,
-                    self.dst_addr)
+                    self.dst_addr))
 
-line = "------------------------------------------------------"
+line = "----------------------------------------------------------------------------------"
+start = time.time()
+
+if not QUIET:
+    debug('Opening Socket for Protocol:{0}'.format(PROTO))
+
 try:
-    start = time.time()
-    sock = None
     sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, PROTO)
     if BIND:
-        sock.bind((HOST, DSTPORTS[0]))
+        bport = DSTPORTS.iterkeys().next()
+        debug("Binding to:'{0}':{1}".format(HOST, bport))
+        sock.bind((HOST, bport))
     pkts = 0
     done = False
     time_remaining = TIMEOUT
@@ -131,6 +146,8 @@ try:
                 sock.settimeout(time_remaining)
             try:
                 data, (ip, info) = sock.recvfrom(65565)
+                sys.stdout.write('#')
+                sys.stdout.flush()
             except socket.timeout:
                 done = True
                 continue
@@ -140,32 +157,52 @@ try:
                                                  data[iphdr.header_len:iphdr.header_len + 4])
                 if SRCPORTS and srcport not in SRCPORTS:
                     continue
-                if dstport and dstport not in DSTPORTS:
+                if DSTPORTS and dstport not in DSTPORTS:
                     continue
             else:
                 srcport = None
                 dstport = None
 
-            if not SRCADDRS or (iphdr.src_addr in SRCADDRS):
-                print line
+            if ((not SRCADDRS or (iphdr.src_addr in SRCADDRS)) and
+                    (not DSTADDRS or (iphdr.dst_addr in DSTADDRS))):
+                if iphdr.src_addr not in results['packets']:
+                    results['packets'][iphdr.src_addr] = {}
+                if iphdr.dst_addr not in results['packets'][iphdr.src_addr]:
+                    results['packets'][iphdr.src_addr][iphdr.dst_addr] = {}
+                if dstport not in results['packets'][iphdr.src_addr][iphdr.dst_addr]:
+                    results['packets'][iphdr.src_addr][iphdr.dst_addr][dstport] = 1
+                else:
+                    results['packets'][iphdr.src_addr][iphdr.dst_addr][dstport] += 1
+                debug(line)
                 iphdr.print_me()
                 if DSTPORTS:
-                    print 'Src Port:{0}, Dst Port:{1}'.format(srcport, dstport)
+                    debug('Src Port:{0}, Dst Port:{1}'.format(srcport, dstport))
                 if not QUIET:
                     dlen = 0
                     plen = len(data)
                     if plen >= iphdr.header_len:
                         data = data[iphdr.header_len:]
                         dlen = len(data)
-                    print 'From:{0}, Pkt:{1}, Data:{2}'.format(ip, plen, dlen)
-                    print 'Info:{0}'.format(info)
-                    print 'Data:{0}'.format(data)
-                print line
+                    debug('From:{0}, Pkt:{1}, Data:{2}'.format(ip, plen, dlen))
+                    debug('Info:{0}'.format(info))
+                    debug('Data:{0}'.format(data))
+                debug(line)
                 pkts += 1
         else:
             done = True
+except KeyboardInterrupt:
+    done = True
 finally:
-    print "Packets:{0}, Time:{1}".format(pkts, time.time() - start)
+    elapsed = "%.2f" % (time.time() - start)
+    results['count'] = pkts
+    results['elapsed'] = float(elapsed)
+    # print "Packets:{0}, Time:{1}".format(pkts, elapsed)
+    out = "\n{0}\n".format(json.dumps(results, indent=4, sort_keys=True))
+    print out
+    if options.resultsfile:
+        with open(options.resultsfile, 'a+') as res_file:
+            res_file.write(out)
+            res_file.flush()
     if sock:
         sock.close()
 
--- cloud_utils/net_utils/ip_tx.py
+++ cloud_utils/net_utils/ip_tx.py
@@ -1,49 +1,6 @@
 #!/usr/bin/python
 """
-
-
-#!/usr/bin/python
-'''
-#
-# Simple SCTP server/listener
-#
-'''
-import socket
-from optparse import OptionParser
-
-
-parser = OptionParser()
-parser.add_option("-p", "--port", dest="port", type="int", default=101,
-                  help="Local SCTP Port to bind to", metavar='PORT')
-
-parser.add_option("-a", "--addr", dest="addr", default='',
-                  help="Local addr to bind to, default is '' or 'listen on all'", metavar='HOST')
-options, args = parser.parse_args()
-
-HOST = options.addr
-PORT = options.port
-#HOST = socket.gethostbyname(socket.gethostname())
-try:
-    conn = None
-    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, 132)
-    s.bind((HOST, PORT))
-    while True:
-        data, (ip, info) = s.recvfrom(1024)
-        plen = len(data)
-        dlen = 0
-        if plen >= 48:
-            data = data[48:]
-            dlen = len(data)
-        print "------------------------------------------------------"
-        print 'From:{0}, Pkt:{1}, Data:{2}'.format(ip, plen, dlen)
-        print 'Data:{0}'.format(data)
-        print "------------------------------------------------------"
-except Exception as E:
-    if conn:
-        conn.close()
-    raise
-if conn:
-    conn.close()
+Simple packet generator/client test tool
 """
 
 import socket
